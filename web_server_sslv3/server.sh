f1077c4c7793c21aa2c303d3402e2ad83c46a141
Eric Lewantowicz <elewanto@gmail.com>
2018-04-06 03:28:29 -0400

    Got some SSLv3 traffic working on the webserver with Python 3.2.6

---
+++ web_server_sslv3/server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Team Password is Password
+# CSE 5473 MitM Project
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/python3.2.6/lib /usr/local/python3.2.6/bin/python3.2 ssl3_server.py
+
+
