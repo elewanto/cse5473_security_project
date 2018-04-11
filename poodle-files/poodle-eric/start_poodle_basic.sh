d41db787e35ea7bf7031b023b170c98cb4689b33
Eric Lewantowicz <elewanto@gmail.com>
2018-04-11 00:46:19 -0400

    update poodle attack cryptographic implementation

--- poodle-files/poodle-eric/start_poodle_basic.sh
+++ poodle-files/poodle-eric/start_poodle_basic.sh
@@ -8,4 +8,4 @@
 #2  :wq!
 
 # use locally installed deprecated Python 2.7.9
-/usr/local/python2.7.9/bin/python2.7 poodle_main.py
+/usr/local/python2.7.9/bin/python2.7 poodle_basic.py 'Cookie: supersecrettoken'
